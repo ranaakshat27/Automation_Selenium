f944d2f43fad80547efa4be6cbd1c28606f9a377
ranaakshat27 <86319948+ranaakshat27@users.noreply.github.com>
2023-07-24 15:06:22 +0530

    Add files via upload

---
+++ Project_Training/src/test/java/Boat_Testbase/Cart_Testbase.java
@@ -0,0 +1,11 @@
+package Boat_Testbase;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class Cart_Testbase {
+	public static WebDriver driver;
+	public static Properties prop;
+
+}
